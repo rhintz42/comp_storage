4e76dbf006d9c3637d6be09ceea687a78803467a
MeowMixer <meowmixer@meowmix-ThinkPad-X220.(none)>
2013-08-01 13:32:27 -0700

    First commit for comp storage repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CompStorage::Application.config.secret_key_base = '82a46d8be2b579b4b128390d0876657a1f9f26424b9d84a7a77b7d3fdf91d6a6f4293474b171ec7d2418dc801062400e003625fde9e03cbb1d5cb7c8b93fb9c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CompStorage::Application.config.session_store :cookie_store, key: '_comp_storage_session'
